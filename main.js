54c54c15017b23b5bc74c90711a130ba4faff809
Wamaitha <wamaithaweru19@gmail.com>
2022-03-06 11:34:40 +0300

    add script.js file

--- main.js
+++
@@ -1,9 +0,0 @@
-
-function calcDate() {
-const days = ["Sundays" , "Monday" , "tuesday", "wednesday" , "thusrday", "friday", "saturday"];
-const d = new Date ();
-let day = days[d.getDay()];
-document.getElementById("bornDay").innerHTML = d.getDay();
-}
-calcDate();
-
---
+++ script.js
@@ -0,0 +1,7 @@
+// collect data from text box
+   // document.getElementById("Lname").value;
+   // console.log(Lname);
+
+// let declare it as a variable
+let lname = document.getElementById("lname").value;
+console.log(lname);
