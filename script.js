b06ccf09e0e875e204559f9ab33c6235e85b68a2
Wamaitha <wamaithaweru19@gmail.com>
2022-03-06 18:26:29 +0300

    add js function in main.html

--- script.js
+++ script.js
@@ -4,8 +4,18 @@
 
 // let declare it as a variable
 let lname = document.getElementById("lname").value;
-const bornDate = document.getElementById("bornDate").value;
+let bornDate = document.getElementById("bornDate").value;
 
-let d = bornDate;
-console.log(d.toString());
+function myDay () {
+    let bornDate = document.getElementById("bornDate").value;
+    let myDay = alert("Calculating You Akan Name");
+}
+
+
+let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+let d = new Date();
+let day = days[d.getDay()];
+document.getElementById("theDay").innerHTML = day;
+
 
